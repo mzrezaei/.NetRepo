8a7a5fa2995fa7e3b1b8e7650ff7d8d3425fac44
Marzieh Rezaei <marziehrezaei930@gmail.com>
2023-12-13 01:15:34 +0330

    Init SelfHost project

---
+++ src/SelfHostServer.Host/OrderController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace SelfHostServer.Host
+{
+    public class OrdersController :ApiController
+    {
+        Order[] orders = new Order[]
+        {
+            new Order{ Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1},
+            new Order{ Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M},
+            new Order{ Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M}
+        };
+        public IEnumerable<Order> GetAllOrders() 
+        { 
+            return orders; 
+        }
+    }
+    //http://localhost:5009/api/products
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product{ Id = 1, Name = "Product 1"},
+            new Product{ Id = 2, Name = "Product 2"},
+            new Product{ Id = 3, Name = "Product 3"}
+        };
+        [HttpGet]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+    }
+}
+public class Order
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public decimal Price { get; set; }
+}
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    
+}
---
+++ src/SelfHostServer.Host/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+namespace SelfHostServer.Host
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new HttpSelfHostConfiguration(baseAddress: "http://localhost:5009");
+            config.Routes.MapHttpRoute("Default", "api/{controller}/{id}", new { id = RouteParameter.Optional });
+
+            HttpSelfHostServer server = new HttpSelfHostServer(config);
+            server.OpenAsync().Wait();
+
+            //just for waiting
+            Console.ReadLine();
+        }
+    }
+}
